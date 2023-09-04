4235a84266873d301552b123a2659e292eddb514
Harshal Jain <harshaljn04@gmail.com>
2023-09-04 10:52:56 +0530

    Update Binary_Search.c

--- Search & Sort/Binary_Search.c
+++ Search & Sort/Binary_Search.c
@@ -15,7 +15,7 @@ int binary_search(int a[],int beg, int end, int x, int n)
         }
         else if(a[mid]>x)
         {
-            beg=mid-1;
+            high=mid-1;
         }
         else
         return -1;
